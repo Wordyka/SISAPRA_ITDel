ee4731da8bf2dd193453b3685319955f0862f046
IndahCSS <82864019+IndahCSS@users.noreply.github.com>
2021-12-02 11:07:53 +0700

    Update Dosen.java

--- src/Pengguna/Dosen.java
+++ src/Pengguna/Dosen.java
@@ -1,4 +1,19 @@
 package Pengguna;
 
 public class Dosen {
+    private String NIDN;
+    
+    public Dosen(String NIDN){
+        this.NIDN = NIDN;
+    }
+    
+    //Setter
+    public void setNIDN(String NIDN){
+        this.NIDN = NIDN;
+    }
+    
+    //Getter
+    public String getNIDN(){
+        return NIDN;
+    }
 }
